5f273fb469e006f672d59e20a015d6ad55c00fec
Alex Close <ajclose@gmail.com>
2017-10-22 20:27:16 -0500

    Create actions, containers, and reducers files

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
 
+import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        HELLO
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="row">
+                <h1>Books N' Roses: Appetite for Instruction</h1>
+                <BookList/>
+                <BookDetail/>
+            </div>
+        );
+    }
 }
 
 export default App;
